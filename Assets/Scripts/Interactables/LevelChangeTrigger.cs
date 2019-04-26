a1868792f5efd46e6c07f11fbf993688a22e1182
Caio Neves <caio.nevesleite@gmail.com>
2019-04-25 21:48:12 -0300

    Level transition animation added

---
+++ Assets/Scripts/Interactables/LevelChangeTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChangeTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string AreaName;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            MasterManager.Instance.SetText(AreaName);
+            MasterManager.Instance.TriggerAnim("LevelTransition");
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/MasterManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MasterManager : MonoBehaviour
+{
+    public static MasterManager Instance;
+    public Animator anim;
+    public Text LevelText;
+    void Awake()
+    {
+        if (Instance != null)
+            GameObject.Destroy(Instance);
+        else
+            Instance = this;
+
+        DontDestroyOnLoad(this);
+    }
+
+    public void TriggerAnim(string triggerName)
+    {
+        anim.SetTrigger(triggerName);
+    }
+
+    public void SetText(string text)
+    {
+        LevelText.text = text;
+    }
+}
